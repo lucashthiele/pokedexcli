ed0274a5b216d7c3df4c3b6c7243be2d69aaf23a
lucashthiele <lucasthiele0@outlook.com>
2024-10-04 21:18:16 -0300

    feat: REPL setup

--- main.go
+++ main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 func main() {
-	fmt.Printf("Bye, World!")
+	commandMap := getCommandMap()
+
+	for {
+		fmt.Printf("pokedex > ")
+		scanner := bufio.NewScanner(os.Stdin)
+		var option string
+		for scanner.Scan() {
+			option = scanner.Text()
+			break
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+
+		command, ok := commandMap[option]
+		if !ok {
+			fmt.Fprintln(os.Stderr, "Unknow Command")
+			callbackHelp()
+		} else {
+			command.callback()
+		}
+	}
+}
+
+func getCommandMap() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    callbackHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits the program",
+			callback:    callbackExit,
+		},
+	}
+}
+
+func callbackHelp() error {
+	commands := getCommandMap()
+	fmt.Printf("\nWelcome to the pokedex\nUsage:\n\n")
+
+	for _, value := range commands {
+		fmt.Printf("%s: %s\n", value.name, value.description)
+	}
+	fmt.Print("\n")
+
+	return nil
+}
+
+func callbackExit() error {
+	os.Exit(0)
+	return nil
 }
