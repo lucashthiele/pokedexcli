76c5bb177ffa497f747bc3302dc252fc30733de3
lucashthiele <lucasthiele0@outlook.com>
2025-05-07 12:51:06 -0300

    fix: improve logs

--- internal/logs/log.go
+++ internal/logs/log.go
@@ -5,7 +5,7 @@ import "fmt"
 func Log(values ...any) {
 	fmt.Printf("-------- Log ---------\n")
 	for _, value := range values {
-		fmt.Printf("%v | ", value)
+		fmt.Printf("%q | ", value)
 	}
 	fmt.Printf("----------------------\n")
 }
